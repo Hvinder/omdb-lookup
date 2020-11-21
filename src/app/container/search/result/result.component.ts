f38281ef3bad063a4f7847d6e58098452b523eb4
Hvinder <harvinder.chavan7@gmail.com>
2020-11-22 01:01:40 +0530

    Added caching

--- src/app/container/search/result/result.component.ts
+++ src/app/container/search/result/result.component.ts
@@ -4,15 +4,12 @@ import { Search } from '../../../types/api-response.type';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.css']
+  styleUrls: ['./result.component.css'],
 })
 export class ResultComponent implements OnInit {
-
   @Input() searchResult: Search = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -11,15 +11,25 @@ import { Search } from '../../types/api-response.type';
 export class SearchComponent implements OnInit {
   name = new FormControl('', [Validators.required, Validators.minLength(4)]);
   searchResults: Search[] = [];
+  searchHistory: string[] = [];
 
   constructor(private searchService: SearchService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.searchResults = JSON.parse(localStorage.getItem('searchResults'));
+  }
 
-  searchResult(searchQuery: string): void {
+  fetchResult(searchQuery: string): void {
+    this.setHistory(searchQuery);
     this.searchService.getResult(searchQuery).subscribe((response) => {
       console.log(response);
       this.searchResults = response.Search;
+      localStorage.setItem('searchResults', JSON.stringify(this.searchResults));
     });
+  }
+
+  setHistory(history: string): void {
+    this.searchHistory.push(history);
+    localStorage.setItem('searchHistory', JSON.stringify(this.searchHistory));
   }
 }
